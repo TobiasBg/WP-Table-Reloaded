586a522e786fa827f1a8b7aaaa94f6d62e3a6e64
google@tobias.baethge.com <google@tobias.baethge.com@335f5990-30c3-11de-96ab-f16d9912eac9>
2009-05-02 11:39:14 +0000

    implemented issue 20 and improved issue 21
    git-svn-id: https://wp-table-reloaded.googlecode.com/svn/trunk@19 335f5990-30c3-11de-96ab-f16d9912eac9

--- wp-table-reloaded-frontend.php
+++ wp-table-reloaded-frontend.php
@@ -21,7 +21,8 @@ class WP_Table_Reloaded_Frontend {
         'table' => 'wp_table_reloaded_data'
     );
     var $shortcode = 'table';
-    
+
+    var $shown_tables = array();
     var $tablesorter_tables = array();
 
     // ###################################################################################################################
@@ -54,13 +55,14 @@ class WP_Table_Reloaded_Frontend {
         // parse shortcode attributs, only allow those specified
         $default_atts = array(
                 'id' => 0,
-                'output_id' => false,
                 'column_widths' => '',
                 'alternating_row_colors' => -1,
                 'first_row_th' => -1,
                 'print_name' => -1,
                 'print_description' => -1,
                 'use_tablesorter' => -1,
+                'row_offset' => 1,
+                'row_count' => null
         );
       	$atts = shortcode_atts( $default_atts, $atts );
 
@@ -85,6 +87,12 @@ class WP_Table_Reloaded_Frontend {
             else
                 $output_options[ $key ] = ( -1 !== $value ) ? $value : $table['options'][ $key ] ;
         }
+        
+        // how often was table displayed on this page yet? get its HTML ID
+        $count = ( isset( $this->shown_tables[ $table_id ] ) ) ? $this->shown_tables[ $table['id'] ] : 0;
+        $count = $count + 1;
+        $this->shown_tables[ $table_id ] = $count;
+        $output_options[ 'html_id' ] = "wp-table-reloaded-id-{$table_id}-no-{$count}";
         
         $output = $this->render_table( $table, $output_options );
 
@@ -120,13 +128,14 @@ class WP_Table_Reloaded_Frontend {
         $cssclasses = array( 'wp-table-reloaded', "wp-table-reloaded-id-{$table['id']}" );
         $cssclasses = implode( ' ', $cssclasses );
 
-        $id_output = ( true == $output_options['output_id'] ) ? " id=\"wp-table-reloaded-id-{$table['id']}\"" : '';
-
         $rows = count( $table['data'] );
         $cols = (0 < $rows) ? count( $table['data'][0] ) : 0;
 
         // make array $shortcode_atts['column_widths'] have $cols entries
         $output_options['column_widths'] = array_pad( $output_options['column_widths'], $cols, '' );
+
+        // if row_offset or row_count were given, we cut that part from the table and show just that
+        $table['data'] = array_slice( $table['data'], $output_options['row_offset'] - 1 , $output_options['row_count'] ); // -1 because we start from 1
 
         $output = '';
 
@@ -135,7 +144,7 @@ class WP_Table_Reloaded_Frontend {
             if ( true == $output_options['print_name'] )
                 $output .= '<h2 class="wp-table-reloaded-table-name">' . $this->safe_output( $table['name'] ) . "</h2>\n";
         
-            $output .= "<table{$id_output} class=\"{$cssclasses}\" cellspacing=\"1\" cellpadding=\"0\" border=\"0\">\n";
+            $output .= "<table id=\"{$output_options['html_id']}\" class=\"{$cssclasses}\" cellspacing=\"1\" cellpadding=\"0\" border=\"0\">\n";
 
             foreach( $table['data'] as $row_idx => $row ) {
                 if ( true == $output_options['alternating_row_colors'] )
@@ -183,15 +192,17 @@ class WP_Table_Reloaded_Frontend {
             if ( true == $output_options['print_description'] )
                 $output .= '<span class="wp-table-reloaded-table-description">' . $this->safe_output( $table['description'] ) . "</span>\n";
 
+            // if alternating row colors, we want to keep those when sorting
             $widgets = ( true == $output_options['alternating_row_colors'] ) ? "{widgets: ['zebra']}" : '';
-            
+
+            // eventually add this table to list of tables which will be tablesorted and thus be included in the script call in wp_footer
             if ( true == $output_options['use_tablesorter'] && true == $output_options['first_row_th'] ) {
                 // check if tablesorter is generally enabled already done
-                
-                // add this table to list of tables which will be tablesorted and thus be included in the script call in wp_footer
-                $this->tablesorter_tables[ $table['id'] ] = $widgets;
+                $this->tablesorter_tables[ $output_options['html_id'] ] = $widgets;
             }
-        }
+            
+        } // endif rows and cols exist
+        
         return $output;
     }
 
@@ -203,7 +214,7 @@ class WP_Table_Reloaded_Frontend {
     // ###################################################################################################################
     // enqueue jquery-js-file
     function add_head_jquery_js() {
-            wp_enqueue_script( 'jquery' );
+        wp_enqueue_script( 'jquery' );
     }
 
     // ###################################################################################################################
@@ -213,41 +224,37 @@ class WP_Table_Reloaded_Frontend {
         $css = ( isset( $this->options['custom_css'] ) ) ? $this->options['custom_css'] : '';
         
         if ( !empty( $css ) ) {
-            $output .= <<<CSSSTYLE
+            echo <<<CSSSTYLE
 <style type="text/css" media="all">
 /* <![CDATA[ */
 {$css}
 /* ]]> */
 </style>
 CSSSTYLE;
-            echo $output;
         }
     }
 
     // ###################################################################################################################
     // output tablesorter execution js for all tables in wp_footer
     function output_tablesorter_js() {
-        if ( 0 < count( $this->tablesorter_tables ) ) {
+        $jsfile =  'jquery.tablesorter.min.js'; // filename of the tablesorter script
+
+        if ( 0 < count( $this->tablesorter_tables ) && file_exists( WP_TABLE_RELOADED_ABSPATH . 'js/' . $jsfile ) ) {
         
             // we have tables that shall be sortable, so we load the js
-            $jsfile =  'jquery.tablesorter.min.js';
-            if ( file_exists( WP_TABLE_RELOADED_ABSPATH . 'js/' . $jsfile ) ) {
-                wp_register_script( 'wp-table-reloaded-tablesorter-js', WP_TABLE_RELOADED_URL . 'js/' . $jsfile, array( 'jquery' ) );
-                wp_print_scripts( 'wp-table-reloaded-tablesorter-js' );
-            }
-        
-            // and echo the specific commands to make them sortable
+            wp_register_script( 'wp-table-reloaded-tablesorter-js', WP_TABLE_RELOADED_URL . 'js/' . $jsfile, array( 'jquery' ) );
+            wp_print_scripts( 'wp-table-reloaded-tablesorter-js' );
+
+            // generate the commands to make them sortable
+            $commands = "\n";
+            foreach ( $this->tablesorter_tables as $html_id => $widgets )
+                $commands .= "\t$(\"#{$html_id}\").tablesorter({$widgets});\n";
+
+            // and echo the commands
             echo <<<JSSCRIPT
 <script type="text/javascript">
 /* <![CDATA[ */
-jQuery(document).ready(function($){\n
-JSSCRIPT;
-
-            foreach ( $this->tablesorter_tables as $table_id => $widgets )
-                echo "\t$(\".wp-table-reloaded-id-{$table_id}\").tablesorter({$widgets});\n";
-    
-            echo <<<JSSCRIPT
-});
+jQuery(document).ready(function($){{$commands}});
 /* ]]> */
 </script>
 JSSCRIPT;
