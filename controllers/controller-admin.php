d257df6ad0009450e551a714eeb383b4b592fc37
Tobias Bäthge <git@tobias.baethge.com>
2012-11-12 18:36:55 +0100

    Fix a notice in WP 3.3 and higher

--- controllers/controller-admin.php
+++ controllers/controller-admin.php
@@ -243,7 +243,10 @@ class WP_Table_Reloaded_Controller_Admin extends WP_Table_Reloaded_Controller_Ba
         $this->add_manage_page_css(); // needs to be added to the header
 
         // done after determining the action, because needs action parameter to load correct help string
-        add_contextual_help( $this->hook, $this->helper->get_contextual_help_string() );
+        if ( version_compare( $GLOBALS['wp_version'] , '3.3', '>' ) )
+			WP_Screen::add_old_compat_help( convert_to_screen( $this->hook ), $this->helper->get_contextual_help_string() );
+		else
+			add_contextual_help( $this->hook, $this->helper->get_contextual_help_string() );
     }
 
     /**
